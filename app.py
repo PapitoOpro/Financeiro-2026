19760a5ee65304c3e72befcba5d486d889cfdbcc
PapitoOpro <souza.rafarodrigues@gmail.com>
2026-06-09 17:10:44 -0300

    Espaçamento h2

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.set_page_config(
 )
 
 st.markdown(
-    "<style>.block-container { padding-top: 2.5rem !important; }</style>",
+    "<style>.block-container { padding-top: 3rem !important; }</style>",
     unsafe_allow_html=True,
 )
 
